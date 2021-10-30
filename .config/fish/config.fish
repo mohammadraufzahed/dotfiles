d302e5c26d035987ce65a50c45dce172c6bd9c5c
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-10-30 09:03:01 +0330

    Added the fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# Set the local bin path
+set -x PATH "/home/morty/.local/bin:$PATH"
+# Set the default editor
+set EDITOR "/bin/nvim"
+# Configs repository
+alias dotfiles="/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
